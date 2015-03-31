2f1150e77e4f43b321bb22c5aa738c8442ac1a7d
larruda <lunascar@gmail.com>
2015-03-31 18:16:52 -0300

    Cron job structure.

---
+++ src/main/java/com/ciandt/gcc/ImportContacts.java
@@ -0,0 +1,17 @@
+package com.ciandt.gcc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ImportContacts extends HttpServlet {
+  
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
+    // TODO: Retrieve all stores User entities
+    // TODO: Loop through User entities and create a task to import contacts
+  }
+}
