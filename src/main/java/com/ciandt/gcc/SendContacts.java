421086d12f2a4de43de1480d77b202e83755640b
Daniel Baldasso <dbaldasso@ciandt.com>
2015-04-10 15:00:54 -0300

    develop: Authorization before SendContacts

--- src/main/java/com/ciandt/gcc/SendContacts.java
+++ src/main/java/com/ciandt/gcc/SendContacts.java
@@ -10,49 +10,57 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONException;
 
 @SuppressWarnings("serial")
-public class SendContacts extends HttpServlet{
+public class SendContacts extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws IOException, ServletException {
+            throws IOException, ServletException {
+
+        
+        final String RestKey = "123";
+        final String key = req.getHeader("Authorization");
+        
+        if (!key.equals(RestKey)) {
+       
+            resp.getWriter().println("Unauthorized");
+            
+        }else{
         
         String reqUser = req.getParameter("contact");
         String setMail = "@";
-        
-        
-        if(reqUser.equals(null)){
-            
+
+        if (reqUser == null) {
+
             resp.getWriter().println("Null Parameter");
-            
-        }else{
-            
+
+        } else {
+
             User user = new User();
-            
-            if(reqUser.contains(setMail)){
 
-           try {
-               resp.getWriter().println(user.QueryContactsAncestor(reqUser));
-           } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            
+            if (reqUser.contains(setMail)) {
 
-            }else if(reqUser.equals("all")){
-                
-            try {
-                resp.getWriter().println(user.QueryContacts());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-                
-            }else{
-                 
+                try {
+                    resp.getWriter().println(
+                            user.QueryContactsAncestor(reqUser));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (reqUser.equals("all")) {
+
+                try {
+                    resp.getWriter().println(user.QueryContacts());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+
                 resp.getWriter().println("Invalid Parameter");
             }
-            
+
         }
-        
-        
-       
-        
     }
+
 }
+}
+
