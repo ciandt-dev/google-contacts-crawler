85578eaf22bf4ecd2ce9dc1492481b6d14cfc82e
larruda <lunascar@gmail.com>
2015-06-02 20:50:53 -0300

    Now using JDO to create Entities on Datastore.

--- src/main/java/com/ciandt/gcc/auth/OAuth2Callback.java
+++ src/main/java/com/ciandt/gcc/auth/OAuth2Callback.java
@@ -2,52 +2,53 @@ package com.ciandt.gcc.auth;
 
 import java.io.IOException;
 
+import javax.jdo.JDOObjectNotFoundException;
+import javax.jdo.PersistenceManager;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ciandt.gcc.PMF;
+import com.ciandt.gcc.entities.User;
 import com.google.api.client.auth.oauth2.*;
 import com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeCallbackServlet;
-import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.users.UserServiceFactory;
 
 public class OAuth2Callback extends AbstractAppEngineAuthorizationCodeCallbackServlet {
-
-  private static final long serialVersionUID = 1L;
-
-  protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential)
-      throws ServletException, IOException {
-
-    String email = UserServiceFactory.getUserService().getCurrentUser().getEmail();
-    long expireTime = (System.currentTimeMillis() / 1000L) + credential.getExpiresInSeconds();
-    
-    Entity user = new Entity("User", email);
-    user.setProperty("accessToken", credential.getAccessToken());
-    user.setProperty("expireTime", expireTime);
-    user.setProperty("refreshToken", credential.getRefreshToken());
-    user.setProperty("imported", false);
-
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(user);
-  }
-
-  @Override
-  protected void onError(
-      HttpServletRequest req, HttpServletResponse resp, AuthorizationCodeResponseUrl errorResponse)
-      throws ServletException, IOException {
-    String nickname = UserServiceFactory.getUserService().getCurrentUser().getNickname();
-    resp.getWriter().print("<h3>" + nickname + ", why don't you want to play with me?</h1>");
-    resp.setStatus(200);
-    resp.addHeader("Content-Type", "text/html");
-  }
-
-  @Override
-  protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
-    return OAuthUtils.getRedirectUri(req);
-  }
-
-  @Override
-  protected AuthorizationCodeFlow initializeFlow() throws IOException {
-    return OAuthUtils.newFlow();
-  }
+  
+    private static final long serialVersionUID = 1L;
+  
+    @Override
+    protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential)
+        throws ServletException, IOException {
+        String email = UserServiceFactory.getUserService().getCurrentUser().getEmail();    
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        
+        try {
+            pm.getObjectById(User.class, email);
+        } catch (JDOObjectNotFoundException exception) {
+            User user = new User(email); 
+            user.setCredential(credential);
+            pm.makePersistent(user);
+            pm.close();
+        }
+    }
+  
+    @Override
+    protected void onError(
+        HttpServletRequest req, HttpServletResponse resp, AuthorizationCodeResponseUrl errorResponse)
+        throws ServletException, IOException {
+        resp.setStatus(200);
+        resp.addHeader("Content-Type", "text/html");
+    }
+  
+    @Override
+    protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
+        return OAuthUtils.getRedirectUri(req);
+    }
+  
+    @Override
+    protected AuthorizationCodeFlow initializeFlow() throws IOException {
+        return OAuthUtils.newFlow();
+    }
 }
--- src/main/java/com/ciandt/gcc/entities/User.java
+++ src/main/java/com/ciandt/gcc/entities/User.java
@@ -1,131 +1,53 @@
-package com.ciandt.gcc;
+package com.ciandt.gcc.entities;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import javax.jdo.annotations.*;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Query;
+@SuppressWarnings("serial")
+@PersistenceCapable
+public class User implements Serializable {
+    @PrimaryKey
+    private String email;
+    
+    @Persistent
+    private boolean imported;
 
-public class User {
-
-    public void setFlagImportedContacts(Key key, boolean valueFlag) {
-
-        try {
-
-            DatastoreService datastore = DatastoreServiceFactory
-                    .getDatastoreService();
-
-            Entity user = datastore.get(key);
-            user.setProperty("imported", valueFlag);
-            datastore.put(user);
-
-        } catch (EntityNotFoundException e) {
-
-            e.printStackTrace();
-        }
-
+    @Persistent(dependent = "true")
+    private Credential credential;
+    
+    @Persistent
+    private List<Contact> contacts = new ArrayList<>();
+  
+    public User(String email) {
+        this.setEmail(email);
     }
 
-    public void setAcessToken(Key key, String newToken, long newTime) {
-
-        try {
-
-            DatastoreService datastore = DatastoreServiceFactory
-                    .getDatastoreService();
-
-            Entity user = datastore.get(key);
-            user.setProperty("accessToken", newToken);
-            user.setProperty("expireTime", newTime);
-            datastore.put(user);
-
-        } catch (EntityNotFoundException e) {
-
-            e.printStackTrace();
-        }
-
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+    
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+        this.imported = true;
     }
 
-    public JSONArray QueryContactsAncestor(String userEmail) throws JSONException {
-
-        DatastoreService datastore = DatastoreServiceFactory
-                .getDatastoreService();
-
-        Key requestUser = KeyFactory.createKey("User", userEmail);
-
-        Query ancestorQuery = new Query("Contact").setAncestor(requestUser);
-        List<Entity> results = datastore.prepare(ancestorQuery).asList(
-                FetchOptions.Builder.withDefaults());
-
-         JSONArray jsonResponseArray = new JSONArray();
-        
-        
-        for(Entity contact : results){
-            
-            JSONObject contactJson = new JSONObject();
-            contactJson.put("name", contact.getProperty("name"));
-            contactJson.put("mail", contact.getKey().getName());
-
-            jsonResponseArray.put(contactJson);
-        }
-        
-        return jsonResponseArray;
+    public String getEmail() {
+        return email;
     }
 
-    public JSONArray QueryContacts() throws JSONException {
 
-        DatastoreService datastore = DatastoreServiceFactory
-                .getDatastoreService();
-
-        Query ancestorQueryUser = new Query("User");
-        List<Entity> results = datastore.prepare(ancestorQueryUser).asList(
-                FetchOptions.Builder.withDefaults());
-        
-        JSONArray jsonResponseArray = new JSONArray();
-        
-        
-        for(Entity user : results){
-            
-            JSONObject userJson = new JSONObject();
-            userJson.put("mail", user.getKey().getName());
-            
-            Query ancestorQueryContacts = new Query("Contact").setAncestor(user.getKey());
-            List<Entity> resultsContacts = datastore.prepare(ancestorQueryContacts).asList(
-                    FetchOptions.Builder.withDefaults());
-            
-            
-            List<JSONObject> list = new ArrayList<JSONObject>();
-            
-            for(Entity contact : resultsContacts){
-                
-                JSONObject contactJson = new JSONObject();
-                
-                contactJson.put("name", contact.getProperty("name"));
-                contactJson.put("mail", contact.getKey().getName());
-                
-                list.add(contactJson);
-              
-            }
-            
-            
-            userJson.put("Contacts", list);
-            
-            jsonResponseArray.put(userJson);
-            
-            
-        }
-
-        return jsonResponseArray;
-
+    public void setEmail(String email) {
+        this.email = email;
     }
 
+    public void setCredential(com.google.api.client.auth.oauth2.Credential credential) {
+        this.credential = new Credential(credential);    
+    }
+
+    public Credential getCredential() {
+        return this.credential;
+    }
 }
