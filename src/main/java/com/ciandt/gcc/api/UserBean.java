54d75bc68be0d7bdd048266d4e63af595142c980
larruda <lunascar@gmail.com>
2015-06-12 00:49:00 -0300

    Indented some lines and added some properties.

--- src/main/java/com/ciandt/gcc/api/UserBean.java
+++ src/main/java/com/ciandt/gcc/api/UserBean.java
@@ -8,23 +8,23 @@ import com.ciandt.gcc.entities.Contact;
 
 @SuppressWarnings("serial")
 public class UserBean implements Serializable {
-  private String email;
+    private String email;
+    
+    private List<Contact> contacts = new ArrayList<>();
   
-  private List<Contact> contacts = new ArrayList<>();
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public List<Contact> getContacts() {
-    return contacts;
-  }
-
-  public void setContacts(List<Contact> contacts) {
-    this.contacts = contacts;
-  }
+    public String getEmail() {
+        return email;
+    }
+  
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+  
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
 }
